e81ec23cf5c12e3936826ef0afc9c21384b68bd0
William Weiskopf <william@weiskopf.me>
2026-03-08 00:22:03 +0100

    Update.

--- devenv.nix
+++ devenv.nix
@@ -20,7 +20,7 @@
     end-of-file-fixer.enable = true;
     deadnix.enable = true;
     flake-checker.enable = true;
-    nixfmt-rfc-style.enable = true;
+    nixfmt.enable = true;
     shellcheck.enable = true;
     statix.enable = true;
     tflint.enable = true;
